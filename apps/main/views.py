619504c1e5970990435ccd22894df57f6125dec7
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-09-04 20:42:53 +0300

    Commit before Cart app

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-
 def view_main_page(request):
-    return render(request,'main/main.html')
+    return render(request,'main/main.html',context={})
 
 def view_contact_page(request):
     return render(request, 'main/contacts.html')
 
-def  detail_page(request):
+def detail_page(request):
     return render(request, 'main/detail.html')
+
+
+
+
