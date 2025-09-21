a8f2887f13eba47f3dc4a5efc17db1369fbae699
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-09-21 10:09:56 +0300

    Some changes in templates and styles

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,1 @@
-from django.db import models
-from django.contrib.sessions.models import Session
 
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
 
 def cart_detail(request):
     return render(request,'cart/cart_detail.html')
